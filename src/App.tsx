f5d81bb5e310ed1a8b878be0fdad7f4243d09c9a
Luigi Maselli <luigi+git@grigio.org>
2019-03-29 10:04:58 +0100

    First EOSUSD tx on localnet!

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,22 @@ class App extends Component {
   componentDidMount() {
 
     ScatterJS.plugins(new ScatterEOS());
+    /// EOS mainnet
+    // const network = ScatterJS.Network.fromJson({
+    //   blockchain: 'eos',
+    //   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+    //   host: 'nodes.get-scatter.com',
+    //   port: 443,
+    //   protocol: 'https'
+    // });
 
+    // EOS Localnet testnet
     const network = ScatterJS.Network.fromJson({
       blockchain: 'eos',
-      chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-      host: 'nodes.get-scatter.com',
-      port: 443,
-      protocol: 'https'
+      chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
+      host: '127.0.0.1',
+      port: 8888,
+      protocol: 'http'
     });
 
     ScatterJS.connect('YourAppName', { network }).then((connected: any) => {
@@ -26,15 +35,32 @@ class App extends Component {
 
       const eos = ScatterJS.eos(network, Eos);
 
-      ScatterJS.login().then((id: any) => {
+      ScatterJS.login().then(async (id: any) => {
         if (!id) { return console.error('no identity'); }
         const account = ScatterJS.account('eos');
         const options = { authorization: [`${account.name}@${account.authority}`] };
-        eos.transfer(account.name, 'safetransfer', '0.0001 EOS', account.name, options).then((res: any) => {
-          console.log('sent: ', res);
-        }).catch((err: any) => {
-          console.error('error: ', err);
+
+        // Custom token transaction, you need testuser already set up and filled with EOSUSD
+        const res = await eos.transaction({
+          actions: [
+            {
+              account: "eosusdeosusd",
+              name: "transfer",
+              authorization: [{
+                actor: account.name,
+                permission: account.authority
+              }
+              ],
+              data: {
+                from: account.name,
+                to: "eosio.token",
+                quantity: "0.0002 EOSUSD",
+                memo: "I'm a memo!"
+              }
+            }]
         });
+
+        console.log('r :', res);
       });
     });
   }
