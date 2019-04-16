41adb2b258b4c48d9df8409a1e0e0e6dd4719d18
Luigi Maselli <luigi+git@grigio.org>
2019-04-16 14:16:30 +0200

    Updates

---
+++ precache-manifest.66b4f4fd519378e10e4bf725599e6789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4513695798edc9c5c954",
+    "url": "/eosusd-ui/static/js/runtime~main.cba63316.js"
+  },
+  {
+    "revision": "ee6144bbf583fc49ab62",
+    "url": "/eosusd-ui/static/js/main.29a837c7.chunk.js"
+  },
+  {
+    "revision": "8e044c78126f0ea2d798",
+    "url": "/eosusd-ui/static/js/2.adb1ffa2.chunk.js"
+  },
+  {
+    "revision": "ee6144bbf583fc49ab62",
+    "url": "/eosusd-ui/static/css/main.567b6782.chunk.css"
+  },
+  {
+    "revision": "8e044c78126f0ea2d798",
+    "url": "/eosusd-ui/static/css/2.9523a1f4.chunk.css"
+  },
+  {
+    "revision": "15bbaeed7d0d19d5ea9856f55c24280d",
+    "url": "/eosusd-ui/index.html"
+  }
+];
